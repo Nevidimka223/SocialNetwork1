591da7259f9d68dd73ad1a762ec144f2c150da86
Nevidimka223 <vinokur-vova@i.ua>
2021-01-12 23:46:38 +0200

    add knex,dicontainer

--- api/app.js
+++ api/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const configService = require('./services/configService');
 
+const DiContainer = require('./services/diContainer');
+const knex = require('knex');
+const dbConnection = knex(configService.getPGCredentials());
+
+DiContainer.register('dbConnection', dbConnection)
+
 const app = express();
 const port = configService.getPort();
+app.use(express.json());
 
 const postsRouter = require('./routes/posts');
+
+app.use('/', postsRouter); 
 
 app.use ('/',(req,res)=>{
   res.send('HI')
 });
-
-app.use('/', postsRouter); 
 
 app.use((err, req, res, next) => {
   res.status(500).send('500: Internal Server Error');
